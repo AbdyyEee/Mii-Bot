2c8d06073a7d8813f89c8868a51f488e5dd4c15a
AbdyyEee <abdyyy123456788@gmail.com>
2022-06-19 15:58:16 +0400

    test

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ load_dotenv('.env')
 TOKEN = os.getenv('TOKEN')
 client = commands.Bot(command_prefix='!')
 
+
 @client.event
 async def on_ready():
     print('Mii-Bot is ready.')
     await client.change_presence(activity=discord.Game('!helpmii'))
 
-# !mii_name command
+
+# !mii_name command.
 for file in os.listdir('cmds\!mii'):
     if file.endswith('.py'):
         client.load_extension(f'cmds.!mii.{file[:-3]}')
