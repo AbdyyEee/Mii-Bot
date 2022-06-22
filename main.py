c94b45a00cca6f6a1561d5b627ffd9c8796cc91e
Abdyy <82438230+AbdyyEee@users.noreply.github.com>
2022-06-22 19:12:13 +0400

    Migrate main.py to version 2.0.0 of discord.py

--- main.py
+++ main.py
@@ -3,20 +3,31 @@ import os
 from discord.ext import commands
 from dotenv import load_dotenv
 
+
+cmds = []
+
+class Mii_Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix='!', 
+            intents=discord.Intents.all()
+        )
+
+        for file in os.listdir('cmds\!mii'):
+            if file.endswith('.py'):
+                cmds.append(f'cmds.!mii.{file[:-3]}')
+
+    async def setup_hook(self):
+        for file in cmds:
+            await self.load_extension(file)
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        await client.change_presence(activity=discord.Game('!helpmii'))
+        print('Bot is logged in... ')
+
+
 load_dotenv('.env')
 TOKEN = os.getenv('TOKEN')
-client = commands.Bot(command_prefix='!')
-
-
-@client.event
-async def on_ready():
-    print('Mii-Bot is ready.')
-    await client.change_presence(activity=discord.Game('!helpmii'))
-
-
-# !mii_name command.
-for file in os.listdir('cmds\!mii'):
-    if file.endswith('.py'):
-        client.load_extension(f'cmds.!mii.{file[:-3]}')
-
+client = Mii_Bot()
 client.run(TOKEN)
